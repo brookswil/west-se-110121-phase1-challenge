7e51f7e080654d5cd19e6d60aee8c2d0e5e5ca5b
Brooks_Wilcox <brookswill88@gmail.com>
2021-11-12 12:03:05 -0700

    submission for deadline

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 // write your code here
+
+// document.querySelector("image").appendChild(Image)
+
+function getImages(){
+    fetch("http://localhost:3000/images/1")
+    .then(res => res.json())
+    .then(data => data.forEach(initialize))
+    console.log("before fetch returns")
+    // console.log(data)
+}
+getImages();
+
+function initialize() {
+    getImages()
+    console.log("after get Images")
+    // imageData.forEach(image => renderOneImage(image))
+
+}
+initialize()
